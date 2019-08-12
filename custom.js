c943ee35488fe83dbb98c3293d1670ae735ca17e
Carlos Trinidad <carlos.trinidad1@ibm.com>
2019-08-11 23:32:45 -0300

    Editado front end para mostrar resultados

--- custom.js
+++ custom.js
@@ -1,45 +1,159 @@
 function searchByText() {
+    $('#response_container').text('').append(
+        `<div class="loader"></div>`
+    );
     var input = document.getElementById('search_bar').value;
     const Http = new XMLHttpRequest();
     const url = 'https://us-south.functions.cloud.ibm.com/api/v1/web/Juan.fajardo1%40ibm.com_dev/default/disco-action-2.json?user_input=' + input;
     Http.open("GET", url);
     Http.send();
 
-    Http.onreadystatechange = (e) => {
-        console.log(Http.responseText);
-        $('#response_container').text(Http.responseText)
+    Http.onreadystatechange = function (e) {
+        if (Http.readyState == 4) {
+            console.log(Http.responseText);
+            $(".loader").fadeOut("slow");
+
+            var json = JSON.parse(Http.responseText);
+
+            $('#response_container').append(
+                `
+                <div class="container">
+                    <div class="row">
+                        <div class="col" id="passages">
+                        </div>
+                        <div class="col" id="documentos">
+                        </div>
+                    </div>
+                </div>
+                `
+            )
+
+            json.passages.forEach(function (element) {
+
+                var passage_text = element.passage_text;
+                var passage_score = element.passage_score;
+
+                $('#passages').append(
+                    `
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Passage
+                            </h5>
+                            <p class="card-text">
+                                ${passage_text} 
+                            </p>
+                            <p>
+                                Score: ${passage_score}
+                            </p>
+                        </div>
+                    </div>
+                    `
+                )
+            })
+
+            json.results.forEach(function (element) {
+
+                var document_text = element.text;
+                var document_score = element.result_metadata.score;
+
+                $('#documentos').append(
+                    `
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Document
+                        </h5>
+                        <p class="card-text">
+                            ${document_text}
+                        </p>
+                        <p>
+                            Score: ${document_score}
+                        </p>
+                    </div>
+                </div>
+                `
+                )
+            });
+        }
     }
 }
 
 function searchTopStories() {
+    $('#response_container').text('').append(
+        `<div class="loader"></div>`
+    );
     var input = document.getElementById('search_bar').value;
     const Http = new XMLHttpRequest();
     const url = 'https://us-south.functions.cloud.ibm.com/api/v1/web/Juan.fajardo1%40ibm.com_dev/default/news-discovery.json?user_input=' + input;
     Http.open("GET", url);
     Http.send();
 
-    Http.onreadystatechange = (e) => {
-        console.log(Http.responseText);
-        $('#response_container').text(Http.responseText)
+    Http.onreadystatechange = function (e) {
+        if (Http.readyState == 4) {
+            console.log(Http.responseText);
+            $(".loader").fadeOut("slow");
+
+            var json = JSON.parse(Http.responseText);
+
+            json.results.forEach(function (element) {
+
+                var main_image_url = element.main_image_url;
+                var title = element.title;
+                var text = element.text;
+                var url = element.url;
+
+                $('#response_container').append(
+                    `
+                <div class="card">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <div class="card-body">
+                                <img src="${main_image_url}" class="card-img" alt="Imagen URL">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    ${title}
+                                </h5>
+                                <p class="card-text">
+                                    ${text} 
+                                </p>
+                                <a href="${url}" target="_blank" class="card-link">
+                                    leer más
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `
+                )
+            });
+        }
     }
 }
 
 function searchBySentiment() {
+    $('#response_container').text('').append(
+        `<div class="loader"></div>`
+    );
     var input = document.getElementById('search_bar').value;
     const Http = new XMLHttpRequest();
     const url = 'https://us-south.functions.cloud.ibm.com/api/v1/web/Juan.fajardo1%40ibm.com_dev/serverless-codeDay/sentimentAnalysis.json?user_input=' + input;
     Http.open("GET", url);
     Http.send();
 
-    Http.onreadystatechange = (e) => {
+    Http.onreadystatechange = function (e) {
         if (Http.readyState == 4) {
             console.log(Http.responseText);
             var json = JSON.parse(Http.responseText);
 
+            $(".loader").fadeOut("slow");
             $('#response_container').append(
                 `
             <h2>Analisis de Sentimiento de: ${input}</h2>
-            <div id="canvas-holder" style="width:100%">
+            <div id="canvas-holder" style="width:80%">
             <canvas id="chart-area"></canvas>
             </div>
             `
